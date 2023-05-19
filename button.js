669f4319d1ad71ec6bced9e611b8efdd695345e6
Mikeg1804 <121838324+Mikeg1804@users.noreply.github.com>
2023-05-18 18:37:54 -0700

    fixedbutton

---
+++ button.js
@@ -0,0 +1,9 @@
+
+// const button = document.getElementById("myButton");
+// const imageInput = document.getElementById('image')
+
+// button.addEventListener("click", () => {
+//     imageInput.style.display = "inline";
+//     button.style.display = "none";
+//     document.getElementById("image").innerText = "Yes";
+//   });
